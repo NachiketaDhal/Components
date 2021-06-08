ea484551f0bebc3a8cd00a90d06b9b310c3a5b34
Nachiketa Dhal <bnachiketa26@gmail.com>
2021-06-08 11:12:04 +0530

    Update dark-mode

--- 20-dark-mode/setup/src/App.js
+++ 20-dark-mode/setup/src/App.js
@@ -1,9 +1,46 @@
-import React, { useState, useEffect } from 'react'
-import data from './data'
-import Article from './Article'
+import React, { useState, useEffect } from "react";
+import data from "./data";
+import Article from "./Article";
+
+const getTheme = () => {
+  let theme = "light-theme";
+  if (localStorage.getItem("theme") === "dark-theme") {
+    theme = "dark-theme";
+  }
+  return theme;
+};
 
 function App() {
-  return <h2>dark mode starter</h2>
+  const [theme, setTheme] = useState(getTheme());
+
+  const handleToggle = () => {
+    if (theme === "light-theme") {
+      setTheme("dark-theme");
+    } else {
+      setTheme("light-theme");
+    }
+  };
+
+  useEffect(() => {
+    document.firstElementChild.className = theme;
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  return (
+    <React.Fragment>
+      <nav className="nav-center">
+        <h1>Overreacted</h1>
+        <button className="btn" onClick={handleToggle}>
+          Toggle
+        </button>
+      </nav>
+      <div className="articles">
+        {data.map((item) => {
+          return <Article key={item.id} item={item} />;
+        })}
+      </div>
+    </React.Fragment>
+  );
 }
 
-export default App
+export default App;
